9d6e32d62f5f6357a38456af55a35f1761d71d5f
Mahmoud Almontasser <almontasser@outlook.com>
2024-04-20 16:39:02 +0200

    add projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,22 +7,21 @@ interface Project {
 }
 
 const projectsData: Project[] = [
-  // {
-  //   title: 'A Search Engine',
-  //   description: `What if you could look up any information in the world? Webpages, images, videos
-  //   and more. Google has many features to help you find exactly what you're looking
-  //   for.`,
-  //   imgSrc: '/static/images/google.png',
-  //   href: 'https://www.google.com',
-  // },
-  // {
-  //   title: 'The Time Machine',
-  //   description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-  //   to the desired date and press "Go". No more worrying about lost keys or
-  //   forgotten headphones with this simple yet affordable solution.`,
-  //   imgSrc: '/static/images/time-machine.jpg',
-  //   href: '/blog/the-time-machine',
-  // },
+  {
+    title: 'Crust Programming Language',
+    description: `This is a hobby project to learn about compilers and language design. I've designed the language to be similar to C and Rust.`,
+    imgSrc: '/static/images/projects/crust.png',
+    href: 'https://github.com/almontasser/crust',
+    repoUrl: 'https://github.com/almontasser/crust',
+  },
+  {
+    title: 'My blog',
+    description:
+      'My 🏡 in the ☁: Featuring my portfolio, showcases, code snippets, development logs, and creative ideas.',
+    imgSrc: '/static/images/projects/blog.png',
+    href: 'https://github.com/almontasser/almontasserly',
+    repoUrl: 'https://github.com/almontasser/almontasserly',
+  },
 ]
 
 export default projectsData
